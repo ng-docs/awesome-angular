10560c919cf921bbe8faef0ca573c50ccc82c86b
Zhicheng Wang <zcwang@thoughtworks.com>
2018-09-27 16:49:02 +0800

    chore: one-key deploy script with SSR prerender

--- deploy-cn.sh
+++ deploy-cn.sh
@@ -1,11 +1,40 @@
 #!/usr/bin/env bash
 
-npm run build
-cp -r ./dist/awesome-angular/* ./prebuilt
-cd ./prebuilt
-cp index.html 404.html
+set -x
+set -e
+
+#npm run build
+
+#npm run build:ssr
+
+kill `lsof -t -i :4000` || true
+
+npm run serve:ssr &
+
+cp -r ./dist/awesome-angular/* ./prebuilt/
+
+cp ./dist/awesome-angular/index.html ./prebuilt/404.html
+
+sleep 3
+
+wget -m --adjust-extension localhost:4000 -P /tmp
+
+kill `lsof -t -i :4000`
+
+cp -r /tmp/localhost:4000/* prebuilt
+
+rm -fr /tmp/localhost:4000
+
+commitMessage=$(git log --oneline -n 1)
+
+cd prebuilt
 
 git add .
-git commit -am "publish"
+git commit -am "${commitMessage}"
 
 git push
+
+cd -
+
+set +x
+set +e
