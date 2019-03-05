c1785a7cea05b4cf8c41810e2b0b35692103423b
Zhicheng WANG <zcwang@thoughtworks.com>
2019-03-05 08:07:05 +0800

    fix: 改用临时目录存放 preview 仓库

--- deploy-cn.sh
+++ deploy-cn.sh
@@ -11,13 +11,13 @@ kill `lsof -t -i :4000` || true
 
 npm run serve:ssr &
 
-rm -fr preview
+rm -fr /tmp/awesome-preview.angular.live
 
-git clone https://asnowwolf:${GITHUB_ACCESS_TOKEN}@github.com/ng-docs/awesome-angular-preview.git preview
+git clone https://asnowwolf:${GITHUB_ACCESS_TOKEN}@github.com/ng-docs/awesome-angular-preview.git /tmp/awesome-preview.angular.live
 
-cp -r ./dist/awesome-angular/* ./preview/
+cp -r ./dist/awesome-angular/* /tmp/awesome-preview.angular.live/
 
-cp ./dist/awesome-angular/index.html ./preview/404.html
+cp ./dist/awesome-angular/index.html /tmp/awesome-preview.angular.live/404.html
 
 sleep 3
 
@@ -25,13 +25,11 @@ wget -m --adjust-extension localhost:4000 -P /tmp
 
 kill `lsof -t -i :4000`
 
-cp -r /tmp/localhost:4000/* preview/
+cp -r /tmp/localhost:4000/* /tmp/awesome-preview.angular.live/
 
 rm -fr /tmp/localhost:4000
 
 commitMessage=$(git log --oneline -n 1)
-
-cd preview
 
 git add .
 git commit -am "${commitMessage}"
