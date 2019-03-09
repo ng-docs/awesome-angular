edf9dff4e6e72584be5857dc1ce674b16a0a9eb0
Zhicheng WANG <zcwang@thoughtworks.com>
2019-03-10 00:27:15 +0800

    fix: 禁用 prerender 的缓存，以免渲染出错误的版本。

--- deploy-cn.sh
+++ deploy-cn.sh
@@ -7,12 +7,6 @@ npm run build
 
 commitMessage=$(git log --oneline -n 1)
 
-rm -fr /tmp/awesome-preview.angular.live
-
-git clone https://asnowwolf:${GITHUB_ACCESS_TOKEN}@github.com/ng-docs/awesome-angular-preview.git /tmp/awesome-preview.angular.live
-
-cp -r ./dist/awesome-angular/* /tmp/awesome-preview.angular.live/
-
 kill `lsof -t -i :4000` || true
 
 npx http-server-spa dist/awesome-angular index.html 4000 &
@@ -22,6 +16,12 @@ rm -fr /tmp/awesome-angular-prerender || true
 npx prerender mirror -r /tmp/awesome-angular-prerender/ http://localhost:4000/
 
 kill `lsof -t -i :4000` || true
+
+rm -fr /tmp/awesome-preview.angular.live
+
+git clone https://asnowwolf:${GITHUB_ACCESS_TOKEN}@github.com/ng-docs/awesome-angular-preview.git /tmp/awesome-preview.angular.live
+
+cp -r ./dist/awesome-angular/* /tmp/awesome-preview.angular.live/
 
 cp -r /tmp/awesome-angular-prerender/localhost:4000/* /tmp/awesome-preview.angular.live/
 
