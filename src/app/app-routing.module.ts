d349c0efe41af1e627d23e5f5df78e8f72eb3628
Zhicheng WANG <zcwang@thoughtworks.com>
2020-01-21 22:21:28 +0800

    fix: 升级到 9.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', initialNavigation: 'enabled' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {
