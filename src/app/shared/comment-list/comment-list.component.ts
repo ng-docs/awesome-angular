2277de0bf232f2df09af3c54e4adbf78b12ed30c
Zhicheng WANG <zcwang@thoughtworks.com>
2020-03-03 08:53:11 +0800

    fix: 把发布仓库更换成个人仓库，以便支持 gitalk

--- src/app/shared/comment-list/comment-list.component.ts
+++ src/app/shared/comment-list/comment-list.component.ts
@@ -18,10 +18,10 @@ export class CommentListComponent implements OnInit {
 
   ngOnInit(): void {
     const instance = new Gitalk({
-        clientID: 'Iv1.41931dc1b14987c5',
-        clientSecret: 'f7040bec377e010ab202d8d0d4589d32a6706610',
-        repo: 'awesome-angular',
-        owner: 'ng-docs',
+        clientID: '202b839143d084549e33',
+        clientSecret: 'fdf2a2b89ddd3d03b7ba23ddd25dfa9f4d993514',
+        repo: 'awesome.angular.cn',
+        owner: 'asnowwolf',
         admin: ['asnowwolf'],
         id: hash.sha1().update(location.pathname).digest('hex'),      // Ensure uniqueness and length less than 50
         distractionFreeMode: false,  // Facebook-like distraction free mode
