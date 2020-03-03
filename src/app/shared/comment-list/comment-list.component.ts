465b6d92f6263745a9984763081f46869040a3fd
Zhicheng WANG <zcwang@thoughtworks.com>
2020-03-03 08:32:27 +0800

    fix: 为 Awesome Angular 配置自己的论坛系统

--- src/app/shared/comment-list/comment-list.component.ts
+++ src/app/shared/comment-list/comment-list.component.ts
@@ -18,10 +18,10 @@ export class CommentListComponent implements OnInit {
 
   ngOnInit(): void {
     const instance = new Gitalk({
-        clientID: '13163246d86291d4c66b',
-        clientSecret: '15386e1378badcfbe33d34f2534fd554f5c3901f',
-        repo: 'blog',
-        owner: 'asnowwolf',
+        clientID: 'Iv1.41931dc1b14987c5',
+        clientSecret: 'f7040bec377e010ab202d8d0d4589d32a6706610',
+        repo: 'awesome-angular',
+        owner: 'ng-docs',
         admin: ['asnowwolf'],
         id: hash.sha1().update(location.pathname).digest('hex'),      // Ensure uniqueness and length less than 50
         distractionFreeMode: false,  // Facebook-like distraction free mode
