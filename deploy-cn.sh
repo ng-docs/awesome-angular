8c1b405758eeb202d1b1fc3c5a724740b773928e
Zhicheng WANG <zcwang@thoughtworks.com>
2020-03-03 09:21:49 +0800

    fix: 修正发布脚本
    (cherry picked from commit d298bc912e5af2682f00abd55ee5daa5df99ad3d)

--- deploy-cn.sh
+++ deploy-cn.sh
@@ -21,6 +21,8 @@ rm -fr /tmp/awesome.angular.live
 
 git clone https://asnowwolf:${GITHUB_ACCESS_TOKEN}@github.com/asnowwolf/blog.git /tmp/awesome.angular.live
 
+cp ./dist/awesome-angular/index.html /tmp/awesome.angular.live/404.html
+
 cp -r ./dist/awesome-angular/* /tmp/awesome.angular.live/
 
 cp -r /tmp/awesome-angular-prerender/localhost:4000/* /tmp/awesome.angular.live/
