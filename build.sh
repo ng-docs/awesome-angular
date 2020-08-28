2bcfd5f4d0484904078828efd64e170bd8636e39
Zhicheng WANG <zcwang@thoughtworks.com>
2020-08-28 20:42:47 +0800

    fix: 使用 github actions 构建并发布

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ npm run build
 
 commitMessage=$(git log --oneline -n 1)
 
-kill `lsof -t -i :4000` || true
+kill $(lsof -t -i :4000) || true
 
 npx http-server-spa dist/awesome-angular index.html 4000 &
 
@@ -15,10 +15,14 @@ rm -fr /tmp/awesome-angular-prerender || true
 
 npx prerender mirror -r /tmp/awesome-angular-prerender/ http://localhost:4000/
 
-kill `lsof -t -i :4000` || true
+kill $(lsof -t -i :4000) || true
 
 cd /tmp/awesome-angular-prerender
 
+git config --global user.email "asnowwolf@gmail.com"
+git config --global user.name "Zhicheng WANG"
+
+git init
 git add .
 git commit -am "${commitMessage}"
 
