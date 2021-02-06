e1b2ff0a83a37e3f4fd3781adc23574a5476725e
Zhicheng WANG <zcwang@thoughtworks.com>
2021-02-06 21:12:41 +0800

    fix: 更换新的 oauth token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  clientId: 'dfa6f18600b59c6cd148',
-  clientSecret: '7ad85c79ff780e427e253c1a94926276fd27d5bf',
+  clientId: '2325215034b0cac90dfd',
+  clientSecret: 'e93f8593548479bfd5d1cec4f5a2a7dcf2a5533e',
   redirectUri: 'https://blog.ralph.wang/comments/landing',
   owner: 'site-wangke',
   repo: 'blog',
