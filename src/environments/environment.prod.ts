ab00b4f79ac1e7c6d351d6f374df3cd4a42dea37
Zhicheng WANG <zcwang@thoughtworks.com>
2021-02-06 20:33:25 +0800

    fix: 修正留言板地址

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   clientId: 'dfa6f18600b59c6cd148',
   clientSecret: '7ad85c79ff780e427e253c1a94926276fd27d5bf',
-  redirectUri: 'https://blog.wangke.io/comments/landing',
+  redirectUri: 'https://blog.ralph.wang/comments/landing',
   owner: 'site-wangke',
   repo: 'blog',
   production: true,
